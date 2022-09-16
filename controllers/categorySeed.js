c3a68ee73e13ce70e9892c33f21878718f939b99
jgqy93 <jgqy93@gmail.com>
2022-09-16 11:23:56 +0800

    categorySeed

---
+++ controllers/categorySeed.js
@@ -0,0 +1,10 @@
+const categorySchema = [
+  { category: "food" },
+  { category: "transport" },
+  { category: "medical" },
+  { category: "shopping" },
+  { category: "personal care" },
+  { category: "gifts" },
+  { category: "house" },
+  { category: "others" },
+];
