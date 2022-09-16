1c314b35d13a20dd5a643883a802c8fa651c9097
jgqy93 <jgqy93@gmail.com>
2022-09-16 11:22:57 +0800

    user seed data

---
+++ controllers/userSeed.js
@@ -0,0 +1,27 @@
+const userSeed = [
+  {
+    name: "Tom",
+    username: "tom123",
+    password: "tom123",
+  },
+  {
+    name: "Jerry",
+    username: "jerry123",
+    password: "jerry123",
+  },
+  {
+    name: "Noah",
+    username: "noah123",
+    password: "noah123",
+  },
+  {
+    name: "Olivia",
+    username: "olivia123",
+    password: "olivia123",
+  },
+  {
+    name: "Emma",
+    username: "emma123",
+    password: "emma123",
+  },
+];
