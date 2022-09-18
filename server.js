d680704562d6139e3ed281fcb69ff844228db419
Justen Manni <mannijusten@gmail.com>
2022-09-19 01:35:26 +0800

    Add GET route in server

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 require("dotenv").config();
-
+//* dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+const Budget = require("./models/budgetSchema");
+
+//* Configurations
 const app = express();
 const port = process.env.PORT ?? 4000;
-
-const mongoose = require("mongoose");
-// const <schema file name> = require("<path to schema>");
-
 const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/fiscal";
+
 mongoose.connect(MONGO_URI);
 mongoose.connection.once("open", () => {
   console.log(`connected to mongo at ${MONGO_URI}`);
 });
 
-// all middleware -> app.use
+//* all middleware -> app.use
 app.use(express.json());
 
 /* ------------------------------------------------------ */
 app.get("/", (req, res) => {
   res.status(200).send("Hi World!");
+});
+
+//* BudgetPage
+app.get("/personal/budget", (req, res) => {
+  res.status(200).send("Hi, You are now in Budget Page");
 });
 
 /* ------------------------------------------------------ */
