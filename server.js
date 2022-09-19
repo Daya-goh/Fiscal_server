3dd5fe209ca27023ff1070a61f824b296a2b99e3
Justen Manni <mannijusten@gmail.com>
2022-09-19 15:17:46 +0800

    Sync changes via git pull orgin main

--- server.js
+++ server.js
@@ -1,47 +1,9 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT ?? 4856;
 const cors = require("cors");
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
 
 const mongoose = require("mongoose");
 // const <schema file name> = require("<path to schema>");
@@ -52,15 +14,14 @@ const Budget = require("./models/budgetSchema");
 
 const MONGO_URI =
   "mongodb+srv://adminfiscal:Passwordfiscal123!@fiscal.q0rwl6l.mongodb.net/test";
-  //* Adding in userController
-  const userController = require("./controllers/UserController"); 
+//* Adding in userController
+const userController = require("./controllers/UserController");
 const User = require("./models/UserSchema");
 
 mongoose.connect(MONGO_URI);
 mongoose.connection.once("open", () => {
   console.log(`connected to mongo at ${MONGO_URI}`);
 });
-
 
 // all middleware -> app.use
 app.use(cors());
@@ -69,13 +30,12 @@ app.use(express.json());
 app.use("/expense", ExpenseRoute);
 app.use("/category", CategoryRoute);
 app.use("/transactions", TransactionRoute);
-app.use("/users", userController); 
+app.use("/users", userController);
 
 /* ------------------------------------------------------ */
 app.get("/", (req, res) => {
   res.status(200).send("Hi World!");
 });
-
 
 //* BudgetPage
 app.get("/personal/budget", (req, res) => {
@@ -84,18 +44,17 @@ app.get("/personal/budget", (req, res) => {
 
 //* For LOGIN
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body; 
-  const user = await User.findOne({ username }); 
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
 
-  if (user === null){
-    res.status(401).send({ error: "No such user found." }); 
-  } else if (bcrypt.compareSync(password, user.password)){
-    res.status(200).send({ msg: "Login successful." }); 
+  if (user === null) {
+    res.status(401).send({ error: "No such user found." });
+  } else if (bcrypt.compareSync(password, user.password)) {
+    res.status(200).send({ msg: "Login successful." });
   } else {
-    res.status(401).send({ error: "Wrong password." }); 
+    res.status(401).send({ error: "Wrong password." });
   }
-})
-
+});
 
 /* ------------------------------------------------------ */
 app.listen(port, () => {
