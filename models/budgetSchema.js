b6734f5046122997adb493088cf5d741e76fbfc4
Justen Manni <mannijusten@gmail.com>
2022-09-19 12:41:36 +0800

    Update budgetSchema

--- models/budgetSchema.js
+++ models/budgetSchema.js
@@ -4,22 +4,22 @@ const budgetSchema = new mongoose.Schema({
   income: [
     {
       title: { type: String, required: true },
-      amount: { type: Number, required: true },
+      amount: { type: mongoose.Types.Decimal128, required: true },
     },
   ],
   fixedExpenditure: [
     {
       title: { type: String, required: true },
-      amount: { type: Number, required: true },
+      amount: { type: mongoose.Types.Decimal128, required: true },
     },
   ],
   savings: [
     {
       title: { type: String, required: true },
-      amount: { type: Number, required: true },
+      amount: { type: mongoose.Types.Decimal128, required: true },
     },
   ],
-  allowance: { type: Number, required: false },
+  allowance: { type: mongoose.Types.Decimal128, required: false },
   recurring: { type: Boolean, required: true },
   active: { type: Boolean, required: true },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: "UserSchema" },
