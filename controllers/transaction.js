97bbe8b60707a15878d3a28a8e8cb2408fd6f33d
jgqy93 <jgqy93@gmail.com>
2022-09-19 14:28:30 +0800

    overview route

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -8,6 +8,18 @@ router.get("/", async (req, res) => {
   res.status(200).send(transactionData);
 });
 
+router.get("/", async (req, res) => {
+  const transactionData = await Expense.find().populate("category");
+  res.status(200).send(transactionData);
+});
+
+router.get("/today", async (req, res) => {
+  const todayTransaction = await Expense.find({
+    date: new Date().toLocaleDateString("en-CA"),
+  });
+  res.send(todayTransaction);
+});
+
 /* ------------------- get transaction by month ------------------- */
 router.get("/:id", async (req, res) => {
   const id = new Date().getMonth() + 1;
